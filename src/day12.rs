0e561d56d66bbf155979e570b855f8cbb9de465d
Francois Perron <fperron@gmail.com>
2023-12-13 22:31:52 -0500

    Day 12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use crate::day12::Spring::{Damaged, Operational, Unknown};
 
 #[cfg(test)]
@@ -72,6 +73,36 @@ mod tests {
 
         assert_eq!(springs.arrangements(), 7169);
     }
+
+    #[test]
+    fn unfolds_row() {
+        let row = Row::from_unfold(".# 1");
+
+        let expected_springs = vec![
+            Operational, Damaged, Unknown,
+            Operational, Damaged, Unknown,
+            Operational, Damaged, Unknown,
+            Operational, Damaged, Unknown,
+            Operational, Damaged,
+        ];
+        assert_eq!(row.springs, expected_springs);
+
+        assert_eq!(row.records, vec![1, 1, 1, 1, 1]);
+    }
+
+    #[test]
+    fn solves_example_part2() {
+        let springs = HotSprings::from_unfold(EXAMPLE_INPUT);
+
+        assert_eq!(springs.arrangements(), 525152);
+    }
+
+    #[test]
+    fn solves_part2() {
+        let springs = HotSprings::from_unfold(&daily_input(12));
+
+        assert_eq!(springs.arrangements(), 1738259948652);
+    }
 }
 
 struct HotSprings {
@@ -81,6 +112,10 @@ struct HotSprings {
 impl HotSprings {
     pub fn from(input: &str) -> HotSprings {
         HotSprings { rows: input.lines().map(Row::from).collect() }
+    }
+
+    pub fn from_unfold(input: &str) -> HotSprings {
+        HotSprings { rows: input.lines().map(Row::from_unfold).collect() }
     }
 
     pub fn arrangements(&self) -> usize {
@@ -102,23 +137,38 @@ impl Row {
         }
     }
 
-    pub fn arrangements(&self) -> usize {
-        self.is_operational(0, 0)
+    pub fn from_unfold(input: &str) -> Row {
+        let (springs, records) = input.split_once(' ').unwrap();
+        Row {
+            springs: [springs; 5].join("?").chars().map(Spring::from).collect(),
+            records: [records; 5].join(",").split(',').map(|v| v.parse().unwrap()).collect(),
+        }
     }
 
-    fn is_operational(&self, spring: usize, record: usize) -> usize {
+    pub fn arrangements(&self) -> usize {
+        let mut cache = Cache::new();
+        self.is_operational(0, 0, &mut cache)
+    }
+
+    fn is_operational(&self, spring: usize, record: usize, cache: &mut Cache) -> usize {
         if spring == self.springs.len() {
             return if record == self.records.len() { 1 } else { 0 };
         }
 
-        match self.springs[spring] {
-            Operational => self.is_operational(spring + 1, record),
-            Damaged => self.is_damaged(spring, record),
-            Unknown => self.is_damaged(spring, record) + self.is_operational(spring + 1, record),
+        if let Some(&arrangement) = cache.get(spring, record) {
+            return arrangement;
         }
+
+        let arrangement = match self.springs[spring] {
+            Operational => self.is_operational(spring + 1, record, cache),
+            Damaged => self.is_damaged(spring, record, cache),
+            Unknown => self.is_damaged(spring, record, cache) + self.is_operational(spring + 1, record, cache),
+        };
+        cache.put(spring, record, arrangement);
+        arrangement
     }
 
-    fn is_damaged(&self, spring: usize, record: usize) -> usize {
+    fn is_damaged(&self, spring: usize, record: usize, cache: &mut Cache) -> usize {
         if record == self.records.len() {
             return 0;
         }
@@ -132,7 +182,7 @@ impl Row {
             return if record == self.records.len() - 1 { 1 } else { 0 };
         }
 
-        self.is_operational(spring_end + 1, record + 1)
+        self.is_operational(spring_end + 1, record + 1, cache)
     }
 
     fn is_a_damaged_group(&self, start: usize, end: usize) -> bool {
@@ -162,4 +212,18 @@ impl Spring {
     }
 }
 
+struct Cache(HashMap<(usize, usize), usize>);
 
+impl Cache {
+    pub fn new() -> Cache {
+        Cache(HashMap::new())
+    }
+
+    pub fn put(&mut self, spring: usize, record: usize, arrangement: usize) {
+        self.0.insert((spring, record), arrangement);
+    }
+
+    pub fn get(&self, spring: usize, record: usize) -> Option<&usize> {
+        self.0.get(&(spring, record))
+    }
+}
