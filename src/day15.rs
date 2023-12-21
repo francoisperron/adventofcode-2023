c3568b3b8814e1d10a8c685d57af23425190e3c7
Francois Perron <fperron@gmail.com>
2023-12-20 19:35:42 -0500

    Day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+use regex::Regex;
+
 #[cfg(test)]
 mod tests {
     use crate::daily_input;
-    use crate::day15::Sequence;
+    use crate::day15::{Boxes, Sequence, Lens};
 
     const EXAMPLE: &str = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 
@@ -26,10 +29,102 @@ mod tests {
         assert_eq!(sequence.hash(), 508498)
     }
 
+    #[test]
+    fn parses_lens() {
+        let lens = Lens::from("rn=1");
+        assert_eq!(lens.label, "rn");
+        assert_eq!(lens.box_index, 0);
+        assert_eq!(lens.operation, '=');
+        assert_eq!(lens.focal_length, 1);
+
+        let lens = Lens::from("qp-");
+        assert_eq!(lens.label, "qp");
+        assert_eq!(lens.box_index, 1);
+        assert_eq!(lens.operation, '-');
+    }
+
+    #[test]
+    fn adds_lens_in_boxes() {
+        let mut boxes = Boxes::new();
+
+        boxes.add_or_replace(Lens::from("rn=1"));
+        boxes.add_or_replace(Lens::from("qp=3"));
+
+        assert_eq!(boxes.boxes[&0], vec![Lens::from("rn=1")]);
+        assert_eq!(boxes.boxes[&1], vec![Lens::from("qp=3")]);
+    }
+
+    #[test]
+    fn updates_lens_in_boxes() {
+        let mut boxes = Boxes::new();
+
+        boxes.add_or_replace(Lens::from("rn=1"));
+        boxes.add_or_replace(Lens::from("cm=2"));
+        boxes.add_or_replace(Lens::from("rn=3"));
+
+        assert_eq!(boxes.boxes[&0], vec![Lens::from("rn=3"), Lens::from("cm=2")]);
+    }
+
+    #[test]
+    fn removes_lens_from_boxes() {
+        let mut boxes = Boxes::new();
+
+        boxes.add_or_replace(Lens::from("pc=4"));
+        boxes.add_or_replace(Lens::from("ot=9"));
+        boxes.add_or_replace(Lens::from("ab=5"));
+        boxes.remove(&Lens::from("pc-"));
+
+        assert_eq!(boxes.boxes[&3], vec![Lens::from("ot=9"), Lens::from("ab=5")]);
+    }
+
+    #[test]
+    fn ignores_removing_from_empty_box() {
+        let mut boxes = Boxes::new();
+
+        boxes.remove(&Lens::from("pc-"));
+
+        assert_eq!(boxes.boxes.get(&3), None);
+    }
+
+    #[test]
+    fn ignores_removing_absent_lens() {
+        let mut boxes = Boxes::new();
+
+        boxes.add_or_replace(Lens::from("ot=9"));
+        boxes.remove(&Lens::from("pc-"));
+
+        assert_eq!(boxes.boxes[&3], vec![Lens::from("ot=9")]);
+    }
+
+    #[test]
+    fn calculates_boxes_focusing_power() {
+        let mut boxes = Boxes::new();
+
+        boxes.add_or_replace(Lens::from("cm=2"));
+        boxes.add_or_replace(Lens::from("ot=7"));
+        boxes.add_or_replace(Lens::from("ot=7"));
+
+        assert_eq!(boxes.focusing_power(), 30);
+    }
+
+    #[test]
+    fn solves_example_part2() {
+        let sequence = Sequence::from(EXAMPLE);
+
+        assert_eq!(sequence.focusing_power(), 145)
+    }
+
+    #[test]
+    #[ignore]
+    fn solves_part2() {
+        let sequence = Sequence::from(&daily_input(15));
+
+        assert_eq!(sequence.focusing_power(), 279116)
+    }
 }
 
 struct Sequence {
-    steps: Vec<String>
+    steps: Vec<String>,
 }
 
 impl Sequence {
@@ -49,5 +144,80 @@ impl Sequence {
 
     fn hash_char(hash: u32, value: u32) -> u32 {
         ((hash + value) * 17) % 256
+    }
+
+    pub fn focusing_power(&self) -> u32 {
+        let mut boxes = Boxes::new();
+
+        for step in self.steps.as_slice() {
+            let lens = Lens::from(step);
+            match lens.operation {
+                '=' => boxes.add_or_replace(lens),
+                '-' => boxes.remove(&lens),
+                _ => panic!()
+            }
+        }
+
+        boxes.focusing_power()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Lens {
+    label: String,
+    box_index: u32,
+    operation: char,
+    focal_length: u32,
+}
+
+impl Lens {
+    pub fn from(step: &str) -> Lens {
+        let regex = Regex::new(r"(?<label>[a-z]+)(?<operation>[=|-])(?<focal_length>[0-9]*)").unwrap();
+        let (_, [label, operation, focal_length]) = regex.captures(step).map(|g| g.extract()).unwrap();
+
+        let box_index = Sequence::hash_string(label);
+        let focal_length = focal_length.parse().unwrap_or(0);
+
+        Lens { label: label.to_string(), box_index, operation: operation.chars().next().unwrap(), focal_length }
+    }
+}
+
+struct Boxes {
+    boxes: HashMap<u32, Vec<Lens>>,
+}
+
+impl Boxes {
+    pub fn new() -> Boxes {
+        Boxes { boxes: HashMap::new() }
+    }
+
+    pub fn add_or_replace(&mut self, lens: Lens) {
+        if let Some(lenses) = self.boxes.get_mut(&lens.box_index) {
+            if let Some(box_index) = lenses.iter().position(|s| s.label == lens.label) {
+                lenses[box_index] = lens;
+            } else {
+                lenses.push(lens);
+            }
+        } else {
+            self.boxes.insert(lens.box_index, vec![lens]);
+        }
+    }
+
+    pub fn remove(&mut self, lens: &Lens) {
+        if let Some(lenses) = self.boxes.get_mut(&lens.box_index) {
+            if let Some(box_index) = lenses.iter().position(|l| l.label == lens.label) {
+                lenses.remove(box_index);
+            }
+        }
+    }
+
+    pub fn focusing_power(&self) -> u32 {
+        self.boxes.iter()
+            .map(|(box_number, lenses)| {
+                lenses.iter().enumerate()
+                    .map(|(box_index, lens)| (box_number + 1) * (box_index as u32 + 1) * lens.focal_length)
+                    .sum::<u32>()
+            })
+            .sum::<u32>()
     }
 }
